9270acaed01f1245fe8482d64bcbd1cecf70c09d
Todd Whitelow <todd@whitelow.tech>
2020-09-17 07:44:51 -0600

    add app component and place header and player into app component

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-function Header() {
+const Header = () => (
+  <header>
+    <h1>Scoreboard</h1>
+    <span className="stats">Players: 1</span>
+  </header>
+);
+
+const Counter = () => (
+  <div className="counter">
+    <button className="counter-action decrement"> - </button>
+    <span className="counter-score">35</span>
+    <button className="counter-action increment"> + </button>
+  </div>
+);
+
+const Player = () => (
+  <div className="player">
+    <span className="player-name">Todd</span>
+    <Counter />
+  </div>
+);
+
+const App = () => {
   return (
-    <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players: 1</span>
-    </header>
-  );
+    <div className="scoreboard">
+      <Header />
+
+      {/* Players list*/}
+      <Player />
+    </div>
+  )
 }
 
+
+
 ReactDOM.render(
-  header,
+  <App />,
   document.getElementById('root')
 )
