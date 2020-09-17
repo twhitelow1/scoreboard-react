70d813ca44c67388fa79d67e43afc9f61dbf96ec
Todd Whitelow <todd@whitelow.tech>
2020-09-17 07:23:50 -0600

    remove old jsx and replace with new header component

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const title = "My First React Element!";
-const desc = "I just learned how to create a react element and append it to the DOM";
-const myTitleId = 'main-title';
-const name = 'Todd';
-
-const header = (
-  <header >
-    <h1 id={myTitleId}> {name}'s First React Element! </h1>
-    <p className="main-desc"> {desc} </p>
-  </header>
-)
+function Header() {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+}
 
 ReactDOM.render(
   header,
